0c12ab880393399313ca1f01820bca4df1b83859
tanwarsachin2803 <tanwarsachin2803@gmail.com>
2023-11-22 16:14:59 -0500

    Committing changes

---
+++ src/main/java/Utilities/ReadFile.java
@@ -0,0 +1,16 @@
+package Utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadFile {
+    Properties prop;
+    public Properties readConfig(String fileName) throws IOException {
+        prop=new Properties();
+        String file=System.getProperty("user.dir")+"//src//main//resources//"+fileName+".properties";
+        FileInputStream fis=new FileInputStream(file);
+        prop.load(fis);
+        return prop;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,18 @@
 package org.example;
 
+import Utilities.ReadFile;
+
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+        Properties prop;
+        ReadFile readFile=new ReadFile();
+        prop=readFile.readConfig("config");
+        System.out.println(prop.get("name"));
+
     }
 }
 
