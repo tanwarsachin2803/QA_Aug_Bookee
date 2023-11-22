dfb3a84502092ab29f608a7263c785d2b47f22e4
tanwarsachin2803 <tanwarsachin2803@gmail.com>
2023-11-22 16:26:02 -0500

    Commit 2

--- src/main/java/Utilities/BaseClass.java
+++ src/main/java/Utilities/BaseClass.java
@@ -1,4 +1,44 @@
 package Utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.io.IOException;
+import java.util.Properties;
+
 public class BaseClass {
+    ReadFile rf=new ReadFile();
+    WebDriver driver;
+    Properties prop;
+    public BaseClass() throws IOException {
+        prop=rf.readConfig("config");
+    }
+
+    public void setup()
+    {
+        String browserName=prop.getProperty("browser");
+        switch (browserName)
+        {
+            default:
+                WebDriverManager.chromedriver().setup();
+                driver=new ChromeDriver();
+                break;
+            case "safari":
+                WebDriverManager.safaridriver().setup();
+                driver=new SafariDriver();
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver=new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver=new EdgeDriver();
+                break;
+        }
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,21 @@
 package org.example;
 
+import Utilities.BaseClass;
 import Utilities.ReadFile;
 
 import java.io.IOException;
 import java.util.Properties;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
         Properties prop;
         ReadFile readFile=new ReadFile();
         prop=readFile.readConfig("config");
         System.out.println(prop.get("name"));
+        BaseClass bs=new BaseClass();
+        bs.setup();
 
     }
 }
